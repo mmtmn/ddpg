79753fe231fbe34077423d67213f347837f0b114
mmtmn <thiagomnobrega@gmail.com>
2023-12-02 23:45:58 -0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import copy
+import random
 
 class PolicyNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -13,13 +16,75 @@ class PolicyNetwork(nn.Module):
         x = torch.tanh(self.fc2(x))  # Assuming the action space is between -1 and 1
         return x
 
-# Assuming some dimensions for the neural network
-input_size = 4  # Example state dimension
-hidden_size = 128  # Example number of neurons in hidden layer
-output_size = 2  # Example number of actions in the action space
+class QNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
-mu = PolicyNetwork(input_size, hidden_size, output_size)
-state_representation = [0.0, 0.0, 0.0, 0.0]  # Example state representation
-s = torch.tensor(state_representation, dtype=torch.float)
+    def forward(self, state, action):
+        x = torch.cat([state, action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
-a_star = mu(s)
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
+
+    def __len__(self):
+        return len(self.buffer)
+
+# Hyperparameters
+input_size = 4
+hidden_size = 128
+output_size = 2
+tau = 0.005  # for soft update of target parameters
+gamma = 0.99  # discount factor
+buffer_size = 1000000
+minibatch_size = 64
+
+# Main model and target model
+policy_net = PolicyNetwork(input_size, hidden_size, output_size)
+target_policy_net = copy.deepcopy(policy_net)
+q_net = QNetwork(input_size + output_size, hidden_size, 1)
+target_q_net = copy.deepcopy(q_net)
+
+# Optimizers
+policy_optimizer = optim.Adam(policy_net.parameters(), lr=1e-4)
+q_optimizer = optim.Adam(q_net.parameters(), lr=1e-3)
+
+# Replay buffer
+replay_buffer = ReplayBuffer(buffer_size)
+
+# Soft update function
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+# Assume we have a function to add noise to the actions for exploration purposes
+def add_exploration_noise(action):
+    # Add exploration noise here (e.g., Ornstein-Uhlenbeck process)
+    return action
+
+# Assume we have a function to interact with the environment and return a new state and reward
+def env_step(state, action):
+    # Implement environment interaction here
+    return next_state, reward, done
+
+# Training loop would go here, including:
+# - Interacting with the environment
+# - Storing transitions in the replay buffer
+# - Sampling from the buffer to update the Q-network and policy network
+# - Periodically updating the target networks using soft_update
